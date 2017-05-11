a7612ecd03843f087dbd25698cb8a5e4b3e8fa8a
ricka7x <ricky.ramirez89@gmail.com>
2017-05-11 17:16:19 -0500

    connected redux and react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import Layout from './shared/layout/Layout'
 import Sidebar from './shared/layout/Sidebar'
 import Main from './shared/layout/Main'
@@ -10,38 +13,38 @@ import SecondaryContent from './shared/layout/SecondaryContent'
 import Donut from './shared/charts/Donut'
 import Logo from './shared/logo/Logo'
 import ToggleSwitch from './shared/switch/ToggleSwitch'
+
+import Home from './components/home/Home'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Layout>
-          <Sidebar>
-            <Logo color='#fff'/>
-            <ToggleSwitch/>
-            <Donut value={75} />
-          </Sidebar>
-          <Main>
-            <Topbar>
+      <Provider store={store}>
+        <Router>
+          <Layout>
+              <Sidebar>
+                <Logo color='#fff'/>
+                <ToggleSwitch/>
+                <Donut value={75} />
+              </Sidebar>
+              <Main>
+                <Topbar>
 
-            </Topbar>
-            <Content>
-              <PrimaryContent>
-                <h1>Hello</h1>
-                <h2>Hello</h2>
-                <h3>Hello</h3>
-                <h4>Hello</h4>
-                <h5>Hello</h5>
-                <h6>Hello</h6>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed pariatur quos error officia assumenda iure nostrum inventore iste esse dolor, numquam quod architecto quaerat rem, facere animi fugit officiis quibusdam cum ratione. Animi nihil quaerat provident beatae nemo repellendus nam rem, maiores nisi fuga aliquam aspernatur dolore voluptate deserunt reprehenderit. Vel illum beatae, neque sit. Dolorem neque beatae odio quas laborum laudantium ad corrupti. Quia reiciendis vitae, rem quidem perspiciatis repudiandae tenetur officiis molestias provident praesentium, harum cumque consequatur necessitatibus beatae velit cupiditate perferendis debitis, maxime? Ad quae corporis deserunt unde, dolor eligendi optio eius enim ratione eveniet. Consectetur porro vero quo nihil adipisci modi minus nam, unde reprehenderit iusto corporis cupiditate, provident dicta quasi tempore nobis voluptates dolor delectus voluptatem rem inventore. Eum natus beatae, libero magni quia iusto quidem aut minima itaque corporis voluptates, explicabo, dolor odio laborum vitae quae animi numquam debitis cupiditate pariatur aliquid suscipit maiores laudantium nulla distinctio. Necessitatibus magnam, tenetur fugit possimus, pariatur dolore quo deserunt distinctio, nulla quis ullam, reiciendis velit non autem commodi dolorem quod ab fugiat! Voluptate debitis rerum laborum quibusdam asperiores illo commodi eius nobis doloremque. Necessitatibus expedita vero saepe corrupti facilis ratione voluptas adipisci distinctio sequi at dolorum nulla sit corporis delectus natus deleniti perspiciatis aspernatur quasi, placeat molestiae numquam omnis non. Fugiat, autem ex, tenetur animi debitis aliquam maiores. Cumque et perferendis fuga, sunt at illo, quod, veniam voluptatibus error consectetur aliquid perspiciatis sapiente quaerat dicta voluptas dolores repudiandae fugit, sint omnis repellendus doloribus laborum excepturi quisquam. Dolorem suscipit, illo ab aperiam possimus, illum omnis, accusamus ipsum dolore facere aliquid molestiae alias! Excepturi vitae quas fugiat numquam adipisci explicabo eius ducimus, rem modi ratione asperiores ea et nisi beatae minima voluptas, ipsa doloribus tenetur incidunt. Aliquam ullam magni provident est voluptates ipsum architecto voluptate, consectetur maiores, nobis quam deserunt rem ab dolores recusandae tempore minima harum excepturi, facere consequatur nam, minus officia. Accusantium, est sint unde quibusdam consequuntur provident assumenda cupiditate molestiae porro earum ipsum excepturi? Earum fugiat neque inventore tempora sunt recusandae, ratione sit officiis ducimus quod libero autem magnam ex eligendi voluptate, beatae porro accusantium ad fugit odio voluptatibus consectetur labore. Architecto distinctio reprehenderit quam sed ipsa in id, rem cum autem, molestiae debitis maiores qui modi totam tenetur velit sapiente aut repellat praesentium repellendus sint consectetur nisi. Facilis vitae et atque earum, voluptate ducimus commodi, quidem quisquam asperiores ad rerum iusto laborum numquam assumenda error? Ipsa sed quibusdam cum! Soluta repellendus veniam est, sunt placeat inventore, reprehenderit assumenda itaque. Porro officiis ullam provident quaerat voluptatibus voluptate odit, maxime eius repellendus similique nisi alias iste incidunt ipsam vitae, ipsum a dolor deserunt eum est libero enim cum ab quidem. Vitae pariatur, deserunt temporibus id fuga quod obcaecati voluptatibus labore delectus ad nemo libero beatae, omnis expedita blanditiis error, quis saepe nobis facere molestiae quos dolorem tempora itaque. Modi rem nulla maiores, quo magni facilis. Illo vero minima atque sapiente magni illum, saepe dignissimos deserunt nostrum earum asperiores, optio pariatur distinctio nisi expedita, quaerat blanditiis quam at harum. Asperiores veritatis quasi dicta id laudantium, delectus repudiandae. Est, facere voluptatem. Possimus officiis mollitia, atque numquam in obcaecati assumenda praesentium voluptatem facilis temporibus dignissimos, neque a magni illum delectus voluptatum, ad voluptate consectetur itaque placeat id! Qui atque odit rem iure ea ipsa fuga repellendus eaque et cum architecto magnam quas quasi alias, autem iste molestiae, ab vitae provident adipisci quaerat veritatis vero aut. Perferendis porro quae vitae cumque qui aut quos culpa, ipsum molestiae atque odio nemo doloremque distinctio placeat, vero enim ratione dolorum id commodi eveniet adipisci. Cum debitis, assumenda eum quae iure id consectetur magnam at cupiditate reiciendis possimus nulla numquam, nobis modi ex corporis! Temporibus a tempore culpa fuga doloribus mollitia commodi quisquam quis animi quaerat! Et qui alias temporibus quo, iste ipsum perspiciatis molestias consequatur voluptate error officia. Saepe deserunt, pariatur ut, asperiores vero voluptates quibusdam natus mollitia repellat voluptatum! Similique mollitia adipisci ullam dignissimos minima tempore nemo. Tenetur doloribus dolorum quam quisquam omnis distinctio veritatis cumque numquam, vel, pariatur, itaque libero cupiditate excepturi ullam! Ut numquam tempora fugiat rerum adipisci commodi ab, vero molestiae veniam praesentium ducimus nostrum, saepe qui suscipit. Accusantium temporibus corrupti reiciendis, molestias, illo illum sequi vel accusamus deleniti, recusandae voluptas aspernatur doloribus, provident! Eligendi praesentium, aut explicabo quos voluptatem. Officia nulla suscipit dolor at provident aliquam corporis nobis praesentium aliquid et neque blanditiis quos magni fugit, facilis minima vero delectus laborum nihil. Fugiat vel reprehenderit autem cumque amet. Modi, suscipit voluptatibus porro voluptas exercitationem a delectus dolorem sequi, voluptates eos commodi minima vitae, praesentium atque quo odit explicabo quaerat magni. Officia sed deserunt omnis. Itaque magni, error pariatur ipsum et culpa, velit, quaerat ipsam atque, nobis hic fuga similique doloremque aperiam. Minus vel, praesentium architecto odit, similique error numquam explicabo doloribus harum dicta, nostrum nobis veniam! Necessitatibus recusandae dolore beatae ea facere. Ut accusantium expedita aut, ducimus aliquid placeat saepe veritatis eum minima reiciendis eaque quos voluptatum itaque, aliquam cum numquam repellat consequatur voluptates incidunt! Illum in aperiam eveniet quis ut, reiciendis minus quam molestiae enim, atque, illo ab minima dolores voluptatum recusandae amet veniam excepturi totam nemo, quas maxime. Voluptas ipsum perferendis odio cumque veritatis, cum, mollitia voluptatibus nisi iste hic. Id nemo, quod eos doloremque distinctio, consequuntur eligendi reiciendis velit. Deleniti id corporis omnis illo reiciendis non odit ullam perspiciatis, dignissimos quaerat quibusdam, animi laboriosam sit illum explicabo doloremque nobis dolorem distinctio officiis fugit rem quis veritatis qui saepe. Autem doloremque provident, soluta vitae itaque aliquid ipsa ex quas alias ad dolore eos. Velit quos nemo, cupiditate ullam ipsa ea beatae id dolorum laborum, magni natus dicta deleniti a ducimus quo quas! Tenetur et assumenda officia, maiores quae, vero obcaecati quis velit deleniti repellendus dolor temporibus dolores. Deleniti laborum accusantium, voluptates, sed veritatis quaerat et aliquid, quia praesentium earum nostrum soluta molestias quisquam iusto deserunt sapiente. Explicabo totam, quod doloribus voluptatum quas nisi culpa sunt eaque, voluptates nostrum possimus consequuntur iure rerum illo, tempore! Vitae animi, rem porro mollitia, qui, magnam esse omnis repellendus non dolor adipisci!
-              </PrimaryContent>
-              <SecondaryContent visible={true}>
+                </Topbar>
+                <Content>
+                  <PrimaryContent>
+                    <Route exact path="/" component={Home}/>
+                  </PrimaryContent>
+                  <SecondaryContent visible={true}>
 
-              </SecondaryContent>
-            </Content>
-          </Main>
-        </Layout>
-    );
+                  </SecondaryContent>
+                </Content>
+              </Main>
+            </Layout>
+          </Router>
+      </Provider>
+    )
   }
 }
 
---
+++ src/reducers.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+
+const mainReducer = combineReducers({
+  form: formReducer
+})
+export default mainReducer
